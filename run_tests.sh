cc264367fb7ccfbc73b4b332fc95eb1ea2ecf749
mrossinek <rmax@ethz.ch>
2020-02-23 16:42:02 +0100

    Properly exit with vader's return code

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage -N --noplugin -u <(cat << VIMRC
+nvim -N --noplugin -u <(cat << VIMRC
 let g:python3_host_prog='./ext/venv/bin/python'
 set rtp+=./ext/vader.vim
 source ./ext/vader.vim/plugin/vader.vim
 set rtp+=.
 source ./plugin/deuterium.vim
 VIMRC
-) -c 'Vader! test/*.vader' > /dev/null
-rm nvim.appimage
+) -c 'Vader! test/*.vader' 2>results.txt
+ret_code=$?
+cat results.txt
+rm results.txt
+exit $ret_code
